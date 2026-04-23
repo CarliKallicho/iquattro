c25032975e51b3785b329d751e114b1ca7b08dc9
CarliPinell <carlos.pinell@processmaker.com>
2026-04-23 00:15:51 -0400

    guardando cambios mobile cronograma

--- app/public/wp-content/themes/iquattro/functions.php
+++ app/public/wp-content/themes/iquattro/functions.php
@@ -10,7 +10,7 @@ if (!defined('ABSPATH')) {
   exit;
 }
 
-define('IQUATTRO_VERSION', '1.3.80');
+define('IQUATTRO_VERSION', '1.4.00');
 define('IQUATTRO_THEME_DIR', get_template_directory());
 define('IQUATTRO_THEME_URI', get_template_directory_uri());
 
